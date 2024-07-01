c37d9861b4a870f2bfac54c1383fe2fb07b31405
DimaOsokin <74316769+DimaOsokin@users.noreply.github.com>
2024-07-01 21:52:17 +0300

    Create data_report_email.py

---
+++ data_report_email.py
@@ -0,0 +1,7 @@
+email_address_K = "email_address_K"
+email_address_M = "email_address_M"
+email_address_O = "email_address_O"
+check_fio_for_send_email_address = ['Имя Фамилия', 'Имя Фамилия', 'Имя Фамилия']
+
+login_email = "login_email"
+pswd_email = "pswd_email"
