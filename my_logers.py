401fa11d91c78c3e0bdc9d5ec62b1c44a00a46aa
DimaOsokin <dimaosokin97@mail.ru>
2025-01-02 09:42:07 +0300

    Каждый день - новый лог файл

--- my_logers.py
+++ my_logers.py
@@ -1,21 +1,22 @@
-import data_report_email
-
+from datetime import datetime
 
 async def log_info(func='log_info', message=None) -> None:
     """
-
+    Запись информации в файл
     :param func: Название функции
     :param message: Описание ошибки
     :return: None
     """
     import logging
 
+    today = datetime.now().strftime('%d.%m.%Y')
+
     # получение пользовательского логгера и установка уровня логирования
     py_logger = logging.getLogger(func)
     py_logger.setLevel(logging.INFO)
 
     # настройка обработчика и форматировщика в соответствии с нашими нуждами
-    py_handler = logging.FileHandler(f"log/info_log.log", mode='a+', encoding='utf-8')
+    py_handler = logging.FileHandler(f"log/info_log_{today}.log", mode='a+', encoding='utf-8')
     py_formatter = logging.Formatter("\n%(name)s %(asctime)s %(levelname)s\n%(message)s",
                                      datefmt='%d.%m.%y %H:%M:%S')
 
@@ -29,19 +30,21 @@ async def log_info(func='log_info', message=None) -> None:
 
 async def log_err(func='log_err', message=None) -> None:
     """
-
+    Запись информации об ошибках в файл
     :param func: Название функции
     :param message: Описание ошибки
     :return: None
     """
     import logging
 
+    today = datetime.now().strftime('%d.%m.%Y')
+
     # получение пользовательского логгера и установка уровня логирования
     py_logger = logging.getLogger(func)
     py_logger.setLevel(logging.ERROR)
 
     # настройка обработчика и форматировщика в соответствии с нашими нуждами
-    py_handler = logging.FileHandler(f"log/error_log.log", mode='a+', encoding='utf-8')
+    py_handler = logging.FileHandler(f"log/error_log_{today}.log", mode='a+', encoding='utf-8')
     py_formatter = logging.Formatter("\n%(name)s %(asctime)s %(levelname)s\n%(message)s",
                                      datefmt='%d.%m.%y %H:%M:%S')
 
@@ -63,6 +66,8 @@ async def alert_message_to_telegram(func, message) -> None:
     :param message: Сообщение об ошибке
     :return: None
     """
+    import data_report_email
+
     from main import bot
     chat_id_chanel = data_report_email.chat_id_chanel
     await bot.send_message(chat_id=chat_id_chanel, text=f"Функция: {func}\nОшибка: {message}")
